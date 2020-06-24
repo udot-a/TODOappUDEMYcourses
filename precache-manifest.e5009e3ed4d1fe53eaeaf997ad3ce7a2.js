324750d883f973e614e41d616242e55657c909e7
udot_a <udot.dron@gmail.com>
2020-06-24 15:11:42 +0300

    Updates

---
+++ precache-manifest.e5009e3ed4d1fe53eaeaf997ad3ce7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e012dc6431d304f3b2bf00dc5998d8",
+    "url": "/TODOappUDEMYcourses/index.html"
+  },
+  {
+    "revision": "bc259c9c0d7a5499e8e2",
+    "url": "/TODOappUDEMYcourses/static/css/main.8e16d8ba.chunk.css"
+  },
+  {
+    "revision": "6945d54cc9c0baeb2679",
+    "url": "/TODOappUDEMYcourses/static/js/2.c9d62512.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TODOappUDEMYcourses/static/js/2.c9d62512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc259c9c0d7a5499e8e2",
+    "url": "/TODOappUDEMYcourses/static/js/main.072012d4.chunk.js"
+  },
+  {
+    "revision": "5a302dec569dd6e444a0",
+    "url": "/TODOappUDEMYcourses/static/js/runtime-main.98e4cfb6.js"
+  }
+]);
